91772f50e3c4faccf7cca0a78e43e78d94e7cf95
frdrk00 <masterkooo@gmail.com>
2023-05-16 21:36:50 +0200

    new repo

---
+++ funtions/routes/products.js
@@ -0,0 +1,135 @@
+const router = require("express").Router();
+const admin = require("firebase-admin")
+const db = admin.firestore()
+
+router.post("/create", async (req, res) => {
+    try {
+        const id = Date.now()
+        const data = {
+            productId: id,
+            product_name: req.body.product_name,
+            product_category: req.body.product_category,
+            product_price: req.body.product_price,
+            imageURL: req.body.imageURL,
+        };
+
+        const response = await db.collection("products").doc(`/${id}/`).set(data)
+        return res.status(200).send({ success: true, data: response });
+
+    } catch (err) {
+        return res.send({ success: false, msg: `Error: ${err}` });
+    }
+})
+
+/* getall the products */
+router.get("/all", async (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection("products");
+            let response = [];
+            await query.get().then(querysnap => {
+                let docs = querysnap.docs;
+                docs.map(doc => {
+                    response.push({...doc.data() });
+                })
+                return response;
+            })
+
+            return res.status(200).send({ success: true, data: response });
+        } catch (err) {
+            return res.send({ success: false, msg: `Error: ${err}` });
+        }
+    })();
+})
+
+
+/* delete a product */
+router.delete("/delete/:productId", async (req, res) => {
+    const productId = req.params.productId;
+    try {
+        await db
+        .collection("products")
+        .doc(`/${productId}/`)
+        .delete()
+        .them((result) => {
+            return res.status(200).send({ success: true, data: result });
+        })
+    } catch (err) {
+        return res.send({ success: false, msg: `Error: ${err}` });
+    }
+})
+
+/* create a cart */
+router.post("/addToCart/:userId" , async (req, res) => {
+    const userId = req.params.userId
+    const productId = req.body.productId
+
+    try {
+        const doc = await db
+            .collection("cartItems")
+            .doc(`/${userId}/`)
+            .collection("items")
+            .doc(`/${productId}/`)
+            .get()
+
+        if (doc.data()) {
+            const quantity = doc.data().quantity + 1
+            const updatedItem = await db
+                .collection("cartItems")
+                .doc(`/${userId}/`)
+                .collection("items")
+                .doc(`/${productId}/`)
+                .update({ quantity })
+            return res.status(200).send({ success: true, data: updatedItem})
+        } else {
+            const data = {
+                productId: productId,
+                product_name: req.body.product_name,
+                product_category: req.body.product_category,
+                product_price: req.body.product_price,
+                imageURL: req.body.imageURL,
+                quantity: 1,
+            }
+
+            const addItems = await db
+                .collection("cartItems")
+                .doc(`/${userId}/`)
+                .collection("items")
+                .doc(`/${productId}/`)
+                .set(data)
+            return res.status(200).send({ success: true, data: addItems})
+        }
+        
+    } catch (err) {
+        return res.send({ success: false, msg: `Error: ${err}` });
+    }
+})
+
+/* get all the cartItems for that user */
+router.get("/getCartItems/:user_id", async (req, res) => {
+    const userId = req.params.user_id;
+    (async () => {
+        try {
+            let query = db
+                .collection("cartItems")
+                .doc(`/${userId}/`)
+                .collection("items")
+            let response = []
+            
+            await query.get()
+            .then((querysnap) => {
+                let docs = querysnap.docs
+
+                docs.map((doc) => {
+                    response.push({ ...doc.data() })
+                })
+                return response
+            })
+            return res.status(200).send({ success: true, data: response })
+        } catch (err) {
+            return res.send({ success: false, msg: `Error: ${err}` })
+        }
+    })()
+})
+
+module.exports = router;
---
+++ funtions/routes/user.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const admin = require("firebase-admin")
+let data = []
+
+router.get("/", (req, res) => {
+    return res.send("Inside the user router");
+});
+
+router.get("/jwtVerification", async (req, res) => {
+    if (!req.headers.authorization) {
+        return res.status(500).send({msg: 'Token Not Found'});
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+    try {
+        const decodedValue = await admin.auth().verifyIdToken(token);
+
+        if (!decodedValue) {
+            return res.status(500).json({ success: false, msg: 'Unauthorized access' });
+        }
+
+        return res.status(200).json({ success: true, data: decodedValue });
+    } catch (err) {
+        return res.send({ success: false, msg: `Error in extracting the token: ${err}`, });
+    }
+});
+
+const listAllUsers = (nextPageToken) => {
+  // List batch of users, 1000 at a time.
+  admin
+    .auth()
+    .listUsers(1000, nextPageToken)
+    .then((listUsersResult) => {
+      listUsersResult.users.forEach((rec) => {
+        data.push(rec.toJSON());
+      });
+      if (listUsersResult.pageToken) {
+        // List next batch of users.
+        listAllUsers(listUsersResult.pageToken);
+      }
+    })
+    .catch((error) => {
+      console.log('Error listing users:', error);
+    });
+};
+// Start listing users from the beginning, 1000 at a time.
+listAllUsers();
+
+router.get("/all", async (req, res) => {
+    listAllUsers()
+    try {
+        return res
+        .status(200)
+        .send({ success: true, data: data, dataCount: data.length })
+    } catch (error) {
+        return res.send({
+            success: false,
+            msg: `Error in listing users: ${error}`
+        })
+    }
+})
+
+
+module.exports = router;
